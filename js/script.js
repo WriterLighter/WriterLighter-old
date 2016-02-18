a281d17ba5d44482036e0b61bbb4424dac392107
Yugo Yagita <2000ygkn0713@gmail.com>
2016-02-19 00:43:17 +0900

    js/script.js綺麗にした

--- js/script.js
+++ js/script.js
@@ -1,86 +1,88 @@
-function wc () {
+function wc() {
 
     var txtvalue = $("#input_txt").text();
 
-        var NoC = txtvalue.length;
-        var BoC = encodeURI(txtvalue).replace(/%[0-9A-F]{2}/g, '*').length;
-        //var line = value.split("\n").length;
-        var line = $("#input_txt").html().split("<br>").length -1 ;
-        line = line + $("#input_txt").html().split("<div>").length -1;
-        line = ++line;
+    var NoC = txtvalue.length;
+    var BoC = encodeURI(txtvalue).replace(/%[0-9A-F]{2}/g, '*').length;
+    //var line = value.split("\n").length;
+    var line = $("#input_txt").html().split("<br>").length - 1;
+    line = line + $("#input_txt").html().split("<div>").length - 1;
+    line = ++line;
 
-        $("#wc").html(NoC + "文字");
-        $("#line").html(line + "行");
-        if( BoC >= 1024 ){
-            $("#byte").html( Math.floor( BoC / 1024) + "キロバイト");
-        }else{
-            $("#byte").html(BoC + "バイト");
-        }}
+    $("#wc").html(NoC + "文字");
+    $("#line").html(line + "行");
+    if (BoC >= 1024) {
+        $("#byte").html(Math.floor(BoC / 1024) + "キロバイト");
+    } else {
+        $("#byte").html(BoC + "バイト");
+    }
+}
 
 $(function () {
 
     /*
-    * 何か入力された時のイベント
-    */
+     * 何か入力された時のイベント
+     */
 
 
     $("#input_txt").keyup(function () {
 
-    var txtvalue = $("#input_txt").text();
+        var txtvalue = $("#input_txt").text();
 
         var NoC = txtvalue.length;
         var BoC = encodeURI(txtvalue).replace(/%[0-9A-F]{2}/g, '*').length;
         //var line = value.split("\n").length;
-        var line = $("#input_txt").html().split("<br>").length　-1;
-        line = line + ($("#input_txt").html().split("<div>").length -1);
-        line = line - ($("#input_txt").html().split("<div><br></div>").length -1);
+        var line = $("#input_txt").html().split("<br>").length　 - 1;
+        line = line + ($("#input_txt").html().split("<div>").length - 1);
+        line = line - ($("#input_txt").html().split("<div><br></div>").length - 1);
         line = ++line;
         //if(line = 0){line = ++line;}
 
         $("#wc").html(NoC + "文字");
         $("#line").html(line + "行");
-        if( BoC >= 1024 ){
-            $("#byte").html( Math.floor( BoC / 1024) + "キロバイト");
-        }else{
+        if (BoC >= 1024) {
+            $("#byte").html(Math.floor(BoC / 1024) + "キロバイト");
+        } else {
             $("#byte").html(BoC + "バイト");
-        }});
+        }
+    });
 
 
     $('div.split-pane').splitPane();
-    });
+});
 
-function toggleWritingMode(){
+function toggleWritingMode() {
     $("#input_txt").toggleClass("write-vertical");
 }
 
-function findAllInText(Str,Keyword){
+function findAllInText(Str, Keyword) {
     var KeywordNum = Keyword.length;
     var start = 0;
     var res = [];
-    while(true){
-        var i = Str.indexOf(Keyword,start);
-        if(i >= 0){
+    while (true) {
+        var i = Str.indexOf(Keyword, start);
+        if (i >= 0) {
             res.push(i);
             start = i + KeywordNum;
-        }else{
+        } else {
             return res;
         }
     }
 }
 
-function insert(Str,insPos,insStr){
-    return Str.slice(0,insPos) + insStr + Str.slice(insPos);
+function insert(Str, insPos, insStr) {
+    return Str.slice(0, insPos) + insStr + Str.slice(insPos);
 }
 
-function SearchAndHighlight(Str,Keyword) {
-    var res=Str;
+function SearchAndHighlight(Str, Keyword) {
+    var res = Str;
     var KeywordNum = Keyword.length;
-    var sres = findAllInText(Str,Keyword);
-    for(var i = sres.length-1 ; i >= 0 ; --i){
-        var start=sres[i];
-        var end=start+KeywordNum;
-        res = insert(res,end,"</span>");
-        res = insert(res,start,"<span class='highlighted'>");
+    var sres = findAllInText(Str, Keyword);
+    for (var i = sres.length - 1; i >= 0; --i) {
+        var start = sres[i];
+        var end = start + KeywordNum;
+        res = insert(res, end, "</span>");
+        res = insert(res, start, "<span class='highlighted'>");
     }
     return res;
 }
