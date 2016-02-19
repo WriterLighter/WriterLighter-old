50e7d566a5af60ff98df604910648970b9391476
Happa8 <okadakoutaro@gmail.com>
2016-02-20 00:22:18 +0900

    バグ修復:検索窓になにも入力されていないときに落ちる不具合修復

--- js/script.js
+++ js/script.js
@@ -87,17 +87,15 @@ function SearchAndHighlight(Str, Keyword) {
     return res;
 }
 
-function Search(){
+function Search() {
     var Keyword = $("#keyword").val();
-    var el = $("#input_txt");
-    var before=el.html();
-    var after=SearchAndHighlight(before,Keyword);
-    el.html(after);
-}
-
-//enterキーでの検索実行関数
-function SearchKey(key){
-    if(key==13){
-        Search();
+    if (!(Keyword == "")) { //検索窓になにも入力されていない時の誤動作を防ぐ !Keywordとかでifできないかと思ったけど無理だった
+        
+        var el = $("#input_txt");
+        var before = el.html();
+        var after = SearchAndHighlight(before, Keyword);
+        el.html(after);
     }
 }
+
+//enterキーでの検索実行関数←いらなかった　次回コミット時にはこのコメントも消します
