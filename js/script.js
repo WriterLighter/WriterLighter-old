05c56a8b471ef8087f46e85d354d3422db0f31e4
Happa8 <okadakoutaro@gmail.com>
2016-02-20 00:43:59 +0900

    UI改良:エディタ部分のmarginをrightから全方向に変更
    こっちのほうが読みやすいと思う

--- js/script.js
+++ js/script.js
@@ -87,6 +87,11 @@ function SearchAndHighlight(Str, Keyword) {
     return res;
 }
 
+//ハイライト除去関数(未実装)
+/*function HighlightRemove{
+    
+}*/
+
 function Search() {
     var Keyword = $("#keyword").val();
     if (!(Keyword == "")) { //検索窓になにも入力されていない時の誤動作を防ぐ !Keywordとかでifできないかと思ったけど無理だった
