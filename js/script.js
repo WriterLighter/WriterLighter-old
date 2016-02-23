2b17118186b70268a970a274aa5ca3ddd497d97f
Happa8 <okadakoutaro@gmail.com>
2016-02-24 00:29:44 +0900

    [modify]超集中モード:超集中モード開始時に、右下に通知

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ var webview;
 $(function () {
     webview = document.getElementById('mainWebview');
 
-     // 何か入力された時のイベント
-
+    // 何か入力された時のイベント
+    
 
     $("#input_txt").keyup(function () {
         count();
@@ -14,22 +14,31 @@ $(function () {
     $('div.split-pane').splitPane();
 
 
+
+
+});
+
+var csObj = new Object();
+csObj.theme= "dark";
+
+$(window).load(function(){
+    $("#container").mCustomScrollbar(csObj);
 });
 
 function toggleWritingMode() {
     console.log(document.getElementById("writingMode").checked);
-if(document.getElementById("writingMode").checked){
-    $("#input_txt").addClass("write-vertical");
-}else{
-    $("#input_txt").removeClass("write-vertical");
-}
+    if (document.getElementById("writingMode").checked) {
+        $("#input_txt").addClass("write-vertical");
+    } else {
+        $("#input_txt").removeClass("write-vertical");
+    }
 }
 
-function toggleWebviewDevTools(){
+function toggleWebviewDevTools() {
     var webview = $("#mainWebview");
-    if(webview.isDevToolsOpened()){
+    if (webview.isDevToolsOpened()) {
         webview.closeDevTools();
-    }else{
+    } else {
         webview.openDevTools();
     }
 }
@@ -38,16 +47,18 @@ function IntensiveMode() {
     $("#right-component").toggle();
     $("#my-divider").toggle();
     $("header").toggle();
-    $("footer").toggle();   
+    $("footer").toggle();
     if ($("#left-component").css("right") == "260px") {
         $("#left-component").css("right", "0px");
         $("#container").css("padding", "0");
+        $.amaran({"message":"超集中モード起動\n解除はF11キー"});
+        BrowserWindow.setFullScreen(true);
     } else {
         $("#left-component").css("right", "260px");
         $("#container").css("padding", "25px 0 25px 0");
     }
-    
-    
-    
+
+
+
 
 }
