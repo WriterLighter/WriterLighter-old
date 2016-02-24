1f53f8500514c8286b1fd8d065b571724ae38fb1
Happa8 <okadakoutaro@gmail.com>
2016-02-24 20:35:44 +0900

    [modify]超集中モードの判定改善
    超集中モードかどうかの変数isIntensiveMode実装

--- js/script.js
+++ js/script.js
@@ -43,22 +43,53 @@ function toggleWebviewDevTools() {
     }
 }
 
+var isIntensiveMode = false;
+
 function IntensiveMode() {
     $("#right-component").toggle();
     $("#my-divider").toggle();
     $("header").toggle();
     $("footer").toggle();
-    if ($("#left-component").css("right") == "260px") {
+    if (!isIntensiveMode) {
         $("#left-component").css("right", "0px");
         $("#container").css("padding", "0");
+        isIntensiveMode = true;
         $.amaran({"message":"超集中モード起動\n解除はF11キー"});
-        BrowserWindow.setFullScreen(true);
     } else {
         $("#left-component").css("right", "260px");
         $("#container").css("padding", "25px 0 25px 0");
+        isIntensiveMode = false; 
     }
+    /*toggleFullScreen($("#left-component"));*/
+    
 
 
 
+}
 
+function toggleFullScreen(elem) {
+    elem = elem[0];
+    if( isFullScreen ){
+        if( document.fullScreen || document.mozFullScreen ||  document.webkitIsFullScreen || document.msFullScreen ) {
+            if (document.cancelFullscreen) {
+                document.cancelFullscreen(); 
+            } else if (document.webkitCancelFullScreen) {
+                document.webkitCancelFullScreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.msCancelFullscreen) {
+                document.msCancelFullscreen(); 
+            }
+        } else {
+            if (elem.requestFullscreen) {
+                elem.requestFullscreen(); 
+            } else if (elem.webkitRequestFullscreen) {
+                elem.webkitRequestFullscreen();
+            } else if (elem.mozRequestFullScreen) {
+                elem.mozRequestFullScreen();
+            } else if (elem.msRequestFullscreen) {
+                elem.msRequestFullscreen(); 
+            }
+        }
+    }
 }
